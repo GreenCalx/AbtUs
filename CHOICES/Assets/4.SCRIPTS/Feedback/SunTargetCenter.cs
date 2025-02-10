18a3576be5e3683894c6c319d1c1a6a2f6bbc8f2
Schoenbm <50584669+Schoenbm@users.noreply.github.com>
2025-02-10 22:49:00 +0100

    SunTargetCenter Fix

--- CHOICES/Assets/4.SCRIPTS/Feedback/SunTargetCenter.cs
+++ CHOICES/Assets/4.SCRIPTS/Feedback/SunTargetCenter.cs
@@ -5,6 +5,10 @@ public class SunTargetCenter : MonoBehaviour
     public Transform player;
     public Transform sun;
 
+    void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
