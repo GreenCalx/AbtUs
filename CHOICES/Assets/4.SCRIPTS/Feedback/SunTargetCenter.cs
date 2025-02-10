fc8ebf8f09a944b577a5f53486c0d5eda240ad74
Schoenbm <50584669+Schoenbm@users.noreply.github.com>
2025-02-10 22:36:40 +0100

    HitboxFeedback + Prefab override

--- CHOICES/Assets/4.SCRIPTS/Feedback/SunTargetCenter.cs
+++ CHOICES/Assets/4.SCRIPTS/Feedback/SunTargetCenter.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class SunTargetCenter : MonoBehaviour
 {
-    public Transform player;
+    private Transform player;
     public Transform sun;
 
 
+    private void Awake()
+    {
+        player = transform.root.transform.Find("Player");
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
